02ae788cac03fd8908c00d577a5a020029e97562
louisjia <ljia@rocketmail.com>
2018-11-16 10:22:33 -0800

    getServerStatus
    getServerStatus

---
+++ getServerStatus.py
@@ -0,0 +1,75 @@
+import http.client
+import ssl
+import base64
+import string
+import json
+
+__author__ = "Louis Jia"
+__copyright__ = "Copyright (C) 2018 Cisco System"
+
+def getRestToken(username, password, serverip):
+  ssl._create_default_https_context = ssl._create_unverified_context
+
+  ##replace server ip address here
+  conn = http.client.HTTPSConnection(serverip)
+
+  payload = "{\"expirationTime\" : 10000000000}\n"
+
+  ## replace user name and password here
+  authenStr="%s:%s" % (username, password)
+
+  base64string = base64.encodestring(bytes(authenStr, 'utf-8'))
+  tmpstr= "Basic %s" % base64string
+  authorizationStr = tmpstr.replace("b\'","").replace("\\n\'","");
+  print(authorizationStr);
+
+  headers = {
+      'content-type': "application/json",
+      'authorization': authorizationStr,
+      'cache-control': "no-cache"
+      }
+
+  conn.request("POST", "/rest/logon", payload, headers)
+
+  res = conn.getresponse()
+  data = res.read()
+  print(data)
+  longstr=data.decode("utf-8")
+  strArr=longstr.split("\"")
+  return strArr[3]
+
+def  getServerStatus(serverip, resttoken):
+  ssl._create_default_https_context = ssl._create_unverified_context
+ 
+  conn = http.client.HTTPSConnection(serverip)
+
+  headers = {
+    'dcnm-token': resttoken,
+    'content-type': "application/x-www-form-urlencoded",
+    'cache-control': "no-cache"
+    }
+
+
+  conn.request("GET", "/fm/fmrest/dbadmin/getServiceList/", headers=headers)
+
+  res = conn.getresponse()
+  data = res.read()
+  print(data)
+  jsonstr=data.decode("utf-8")
+  decoded = json.loads(jsonstr)
+
+
+  for x in decoded :
+    print('Service:'+ x['Service'] + 'Status' +x['Status'])
+
+  return
+
+
+# DCNM username, password, DCNM server ip address
+restToken=getRestToken("admin", "xxxxxxx", "10.157.34.138")
+print(restToken)
+
+# DCNM server ip, resetTotken
+getServerStatus("10.157.34.138",restToken)
+
+
